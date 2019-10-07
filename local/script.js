98f792190f6ebf570fbe8362dc1880d95f1ba5f5
Jonah Fidel <jonahfidel@gmail.com>
2019-10-07 18:44:26 -0400

    back to original functionality.

--- local/script.js
+++ local/script.js
@@ -5,35 +5,39 @@
 
 window.onload = initAll;
 function initAll(){
-    var mySVG = document.getElementById("map");
-    console.log(mySVG);
-    var svgDoc = mySVG.getSVGDocument();
-    console.log(svgDoc);
-    var alaska = svgDoc.getElementById("AK");
-    console.log(alaska);
+  var mySVG = document.getElementById("map");
+  console.log(mySVG);
+  var svgDoc = mySVG.getSVGDocument();
+  console.log(svgDoc);
+  var alaska = svgDoc.getElementById("AK");
+  console.log(alaska);
+
+ var myMap = $('#map').get(0).getSVGDocument();
+ var myPath = $(myMap).find("path, circle");
+ $(myPath).hover(function(e) {
+    $('#info-box').css('display','block');
+    $('#info-box').html($(this).data('info'));
+  });
+
+  $(myPath).mouseleave(function(e) {
+    $('#info-box').css('display','none');
+  });
+
+  $(myMap).mousemove(function(e) {
+    $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+    $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+  }).mouseover();
+
+  var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  if(ios) {
+    $('a').on('click touchend', function() {
+      var link = $(this).attr('href');
+      window.open(link,'_blank');
+      return false;
+    });
+  }
 }
-  
 
-// $("path, circle").hover(function(e) {
-//   $('#info-box').css('display','block');
-//   $('#info-box').html($(this).data('info'));
-// });
 
-// $("path, circle").mouseleave(function(e) {
-//   $('#info-box').css('display','none');
-// });
 
-// $(document).mousemove(function(e) {
-//   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-//   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-// }).mouseover();
-
-// var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-// if(ios) {
-//   $('a').on('click touchend', function() {
-//     var link = $(this).attr('href');
-//     window.open(link,'_blank');
-//     return false;
-//   });
-// }
 
