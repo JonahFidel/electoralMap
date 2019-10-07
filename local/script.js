97a290dfabedac88dba2f07fe6faeefbb99949f4
Jonah Fidel <jonahfidel@gmail.com>
2019-10-07 17:28:13 -0400

    embeded svg works.

--- local/script.js
+++ local/script.js
@@ -1,22 +1,34 @@
-$("path, circle").hover(function(e) {
-  $('#info-box').css('display','block');
-  $('#info-box').html($(this).data('info'));
-});
-
-$("path, circle").mouseleave(function(e) {
-  $('#info-box').css('display','none');
-});
-
-$(document).mousemove(function(e) {
-  $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-  $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-}).mouseover();
-
-var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if(ios) {
-  $('a').on('click touchend', function() {
-    var link = $(this).attr('href');
-    window.open(link,'_blank');
-    return false;
-  });
+window.onload = initAll;
+function initAll(){
+    var mySVG = document.getElementById("map");
+    console.log(mySVG);
+    var svgDoc = mySVG.getSVGDocument();
+    console.log(svgDoc);
+    var alaska = svgDoc.getElementById("AK");
+    console.log(alaska);
 }
+  
+
+// $("path, circle").hover(function(e) {
+//   $('#info-box').css('display','block');
+//   $('#info-box').html($(this).data('info'));
+// });
+
+// $("path, circle").mouseleave(function(e) {
+//   $('#info-box').css('display','none');
+// });
+
+// $(document).mousemove(function(e) {
+//   $('#info-box').css('top',e.pageY-$('#info-box').height()-30);
+//   $('#info-box').css('left',e.pageX-($('#info-box').width())/2);
+// }).mouseover();
+
+// var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+// if(ios) {
+//   $('a').on('click touchend', function() {
+//     var link = $(this).attr('href');
+//     window.open(link,'_blank');
+//     return false;
+//   });
+// }
+
